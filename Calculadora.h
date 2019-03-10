61984301a07e62d5fb5d96739ef841a50b769462
Isaac Ortega <irsacfac@estudiantec.cr>
2019-03-10 14:15:34 -0600

    prueba

---
+++ Calculadora.h
@@ -0,0 +1,11 @@
+#pragma once
+class Calculadora {
+public:
+	Calculadora();
+	sumar(int num1, int num2);
+}
+
+int Calculadora::sumar(int num1, int num2) {
+	int result = num1 + num2;
+	return result
+}
